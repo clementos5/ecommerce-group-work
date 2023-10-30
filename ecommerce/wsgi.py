0326fc093c58238debbc6bf5d5e1846896b6a19c
clementos5 <clemalex00@gmail.com>
2023-10-30 13:50:41 +0200

    second commit on wsqi.py

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'ecommerce.settings'
+settings_module = 'ecommerce.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'ecommerce.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
